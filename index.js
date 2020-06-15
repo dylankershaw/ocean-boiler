cd707f17b5161ab69d540a165a50223a1ab311c8
dylankershaw <dylankershaw@me.com>
2020-06-14 21:45:10 -0400

    finish core functionality and layout

--- index.js
+++ index.js
@@ -10,18 +10,53 @@ function stopCounter() {
   clearInterval(counter);
 }
 
+function counterEl() {
+  return document.querySelector('.counter');
+}
+
+function resetCounter() {
+  stopCounter();
+  counterEl().innerText = '$0';
+  cost = 0;
+}
+
 function updateCost() {
-  const costEl = document.getElementById('cost');
   const costPerSecond = getCostPerSecond();
   cost = cost + costPerSecond;
-  costEl.innerText = cost.toFixed(2);
+  counterEl().innerText = '$' + (+cost.toFixed(2)).toLocaleString();
 }
 
 function getCostPerSecond() {
   const hoursWorkedPerYear = 40 * 52;
-  const salaries = document.getElementsByClassName('salary');
+  const salaries = document.querySelectorAll('.salary input');
+
   return [...salaries].reduce((total, {value}) => {
     const salaryPerSecond = +value / hoursWorkedPerYear / 60 / 60;
     return total + salaryPerSecond;
   }, 0);
+}
+
+function toggleSalariesVisibility() {
+  const salaries = document.getElementsByClassName('salaries')[0];
+  const salariesButton = document.getElementsByClassName('salaries-button')[0];
+
+  if (salaries.classList.contains('salaries--closed')) {
+    salaries.classList.remove('salaries--closed');
+    salariesButton.innerText = '-';
+  } else {
+    salaries.classList.add('salaries--closed');
+    salariesButton.innerText = '+';
+  }
+}
+
+function addSalary() {
+  const lastSalary = document.querySelector('.salary:last-of-type');
+  const newSalary = lastSalary.cloneNode(true);
+  lastSalary.after(newSalary);
+}
+
+function removeSalary() {
+  const numberOfSalaries = document.getElementsByClassName('salary').length;
+  if (numberOfSalaries === 1) return;
+  event.target.parentElement.remove();
 }
